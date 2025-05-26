a4151224044889824bd664446caae5565f77e5e0
Nicolas <nicolasbakanowski@gmail.com>
2025-05-26 18:42:02 -0300

    delete double spinner loader

--- src/components/sections/Properties.tsx
+++ src/components/sections/Properties.tsx
@@ -1,20 +1,9 @@
 "use client";
-
-import { useSelector } from "react-redux";
-import { RootState } from "@/store";
 import PropertiesCard from "@/components/ui/cards/PropertiesCard/ProperitesCard";
-import Spinner from "@/components/ui/spinners/Spinner";
+import { Property } from "@/types/property"
+import { Zone } from "@/types/zone";
 
-export default function Properties() {
-  const {
-    data: zones,
-    loading,
-    error,
-  } = useSelector((state: RootState) => state.zones);
-
-  if (loading) return <Spinner />;
-  if (error) return <p className="text-center text-red-500">Error: {error}</p>;
-
+export default function Properties({ zones }: { zones: Zone[] }) {
   return (
     <section className="max-w-6xl bg-dozebg2 mx-auto">
       <div className="text-center bg-greenlight rounded-t-xl mb-1 py-4 px-2">
@@ -26,7 +15,7 @@ export default function Properties() {
         </p>
       </div>
       {zones?.map((zone) =>
-        zone.properties?.map((property) => (
+        zone.properties?.map((property:Property) => (
           <PropertiesCard key={property.id} {...property} zone={zone.name} />
         ))
       )}
--- src/components/sections/Zones.tsx
+++ src/components/sections/Zones.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getZones } from "@/store/zoneSlice";
-import type { AppDispatch, RootState } from "@/store";
 import ZoneCard from "../ui/cards/ZoneCard/ZoneCard";
 import { extractPoints } from "@/utils/mapUtils/extractPoints";
 import { parseAreaToCoordinates } from "@/utils/mapUtils/parseAreaToCoordiantes";
-import Spinner from "@/components/ui/spinners/Spinner";
-export default function ZoneSection() {
-  const dispatch = useDispatch<AppDispatch>();
-  const {
-    data: zones,
-    loading,
-    error,
-  } = useSelector((state: RootState) => state.zones);
-
-  useEffect(() => {
-    dispatch(getZones());
-  }, [dispatch]);
-
-  if (loading) return <Spinner />;
-
-  if (error) return <p className="text-center text-red-500">Error: {error}</p>;
-
+import { Zone } from "@/types/zone";
+ 
+export default function ZoneSection({ zones }: { zones: Zone[] }) {
   return (
     <div className="relative py-10">
       <div className="absolute inset-0 -z-10 bg-dozebg2" />
