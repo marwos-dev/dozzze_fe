f022f8c1beec8eee381eed9fc27389030c283d72
Nicolas <nicolasbakanowski@gmail.com>
2025-06-04 22:33:07 -0300

    visual changes in property page

--- src/store/selectors/roomsSelectors.ts
+++ src/store/selectors/roomsSelectors.ts
@@ -6,3 +6,20 @@ export const selectRoomsForProperty = (propertyId: number) =>
     (state: RootState) => state.rooms.roomsByProperty[propertyId],
     (rooms) => rooms ?? []
   );
+
+const emptyArray: any[] = [];
+
+export const selectFilteredRoomsForProperty = (
+  propertyId: number,
+  capacity?: number,
+  category?: string
+) =>
+  createSelector(
+    (state: RootState) => state.rooms.roomsByProperty[propertyId] || emptyArray,
+    (rooms) =>
+      rooms.filter((room) => {
+        const capacityMatch = capacity ? room.pax >= capacity : true;
+        const categoryMatch = category ? room.type === category : true;
+        return capacityMatch && categoryMatch;
+      })
+  );
