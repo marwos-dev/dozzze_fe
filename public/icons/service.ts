f3993e58b661af4df98af15fd2e38e0c3cb2b15e
Nicolas <nicolasbakanowski@gmail.com>
2025-07-24 02:39:55 -0300

    add svg icons for roomtype

---
+++ public/icons/service.ts
@@ -0,0 +1,56 @@
+export const includedServicesMock = [
+  {
+    name: 'Wifi gratis',
+    message: 'Conexión wifi rápida sin costo adicional',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 16.5a5.5 5.5 0 017 0M5 13a10 10 0 0114 0M12 20h.01" />
+    </svg>`,
+  },
+  {
+    name: 'Desayuno incluido',
+    message: 'Desayuno continental todas las mañanas',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+    </svg>`,
+  },
+  {
+    name: 'Estacionamiento gratuito',
+    message: 'Estacionamiento privado incluido',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17v-2a2 2 0 012-2h4a2 2 0 012 2v2M5 17h14M6 10h12l1 5H5l1-5z" />
+    </svg>`,
+  },
+  {
+    name: 'Piscina',
+    message: 'Piscina al aire libre disponible',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 18s1-2 4-2 4 2 4 2 1-2 4-2 4 2 4 2" />
+    </svg>`,
+  },
+  {
+    name: 'Aire acondicionado',
+    message: 'Todas las habitaciones tienen A/C',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l3-3 3 3M12 4v13" />
+    </svg>`,
+  },
+  {
+    name: 'TV por cable',
+    message: 'TV con canales por cable',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <rect x="3" y="5" width="18" height="12" rx="2" ry="2" stroke-width="2" />
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 21h8" />
+    </svg>`,
+  },
+  {
+    name: 'Mascotas permitidas',
+    message: 'Tu mascota es bienvenida',
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <circle cx="5.5" cy="10.5" r="1.5" />
+      <circle cx="18.5" cy="10.5" r="1.5" />
+      <circle cx="9" cy="5" r="2" />
+      <circle cx="15" cy="5" r="2" />
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13c-4 0-6 3-6 4.5S9 21 12 21s6-1.5 6-3-2-4.5-6-4.5z" />
+    </svg>`,
+  },
+];
